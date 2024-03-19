33e01e5315aa45f2ed38f8fb96600bf0ca872f67
oserna1 <oserna1994@gmail.com>
2024-03-18 19:31:09 -0500

    Initial Commit

---
+++ set/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  set
+//
+//  Created by Oscar Serna on 3/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ set/setApp.swift
@@ -0,0 +1,17 @@
+//
+//  setApp.swift
+//  set
+//
+//  Created by Oscar Serna on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct setApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
