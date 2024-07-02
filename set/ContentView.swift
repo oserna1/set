0e2a718af220ed1e69e0ecbef895841c2789116f
oserna1 <oserna1994@gmail.com>
2024-07-01 19:47:35 -0500

    extracted aspect vgrid

--- set/ContentView.swift
+++ set/ContentView.swift
@@ -25,57 +25,9 @@ struct ContentView: View {
             }
         }
     }
-    
-    struct AspectVGrid<Item: Identifiable, ItemView: View>: View {
-        let items: [Item]
-        var aspectRatio: CGFloat = 1
-        let content: (Item) -> ItemView
-        
-        init(_ items: [Item], aspectRatio: CGFloat, @ViewBuilder content: @escaping (Item) -> ItemView) {
-            self.items = items
-            self.aspectRatio = aspectRatio
-            self.content = content
-        }
-        
-        var body: some View {
-            GeometryReader { geometry in
-                let gridItemSize = gridItemWidthThatFits(
-                    count: items.count,
-                    size: geometry.size,
-                    atAspectRatio: aspectRatio
-                )
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                    ForEach(items) { item in
-                        content(item).aspectRatio(aspectRatio, contentMode: .fit)
-                    }
-                }
-            }
-            .padding(.horizontal)
-        }
-        
-        private func gridItemWidthThatFits(
-            count: Int,
-            size: CGSize,
-            atAspectRatio aspectRatio: CGFloat
-        ) -> CGFloat {
-            let count = CGFloat(count)
-            var columnCount = 1.0
-            repeat {
-                let width = size.width / columnCount
-                let height = width / aspectRatio
-                
-                let rowCount = (count / columnCount).rounded(.up)
-                if rowCount * height < size.height {
-                    return (size.width / columnCount).rounded(.down)
-                }
-                columnCount += 1
-            } while columnCount < count
-            return min(size.width / count, size.height * aspectRatio).rounded(.down)
-        }
-    }
 }
 
-func getColor(fromString color: String) -> Color {
+public func getColor(fromString color: String) -> Color {
     if (color == "red") {
         return Color.red
     } else if (color == "green") {
@@ -95,7 +47,7 @@ public func getShape(fromString shape: String) -> some Shape {
     }
 }
 
-func getBorderColor(selectType: Select) -> Color {
+public func getBorderColor(selectType: Select) -> Color {
     switch selectType {
     case .selected(true):
         .yellow
